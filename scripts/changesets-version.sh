00897b67236b52a93683d6cdcce05caf5ccc04b9
Leo Ribeiro <leordev@gmail.com>
2024-03-20 16:42:48 -0400

    fix api release

--- scripts/changesets-version.sh
+++ scripts/changesets-version.sh
@@ -5,23 +5,23 @@ IGNORE_API_RELEASE=""
 # force api release if argument is passed
 FORCE_API_RELEASE=$1
 
-# check if api release is forced or if there 
+# check if api release is forced through arg or if there 
 # is a changeset PR with the api-release label
-API_RELEASE_PR=$FORCE_API_RELEASE || $(gh pr list \
-                --base 'main' \
-                --head 'changeset-release/main' \
-                --json title,labels,number \
-                --jq '[.[] | select(.labels[]?.name == "api-release")] | first | .number')
+if [[ -n "$FORCE_API_RELEASE" ]]; then
+    echo "API Release forced through argument"
+fi
 
 # if there is no PR, then we release without the Web5 API
-if [[ -z "$API_RELEASE_PR" ]]; then
+if [[ -z "$FORCE_API_RELEASE" ]]; then
     echo "This is a release without the Web5 API"
     IGNORE_API_RELEASE="--ignore @leordev-web5/api"
 else
     echo "Web5 API Release DETECTED"
 fi
 
-pnpm changeset version $IGNORE_API_RELEASE
+CMD="pnpm changeset version $IGNORE_API_RELEASE"
+echo "Running: $CMD"
+$CMD
 
 # update pnpm-lock.yaml
 pnpm install --no-frozen-lockfile
